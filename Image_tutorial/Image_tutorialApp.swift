27e1dff4087163329df5551b871c0ae8a21d8bc8
keemeesuu <kimhogy@gmail.com>
2021-05-20 20:51:40 +0900

    Initial Commit

---
+++ Image_tutorial/Image_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_tutorialApp.swift
+//  Image_tutorial
+//
+//  Created by hogy on 2021/05/20.
+//
+
+import SwiftUI
+
+@main
+struct Image_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
